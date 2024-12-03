24dbb47ff6e4aa17594e1ee881592d30c56acc2a
MakFil <makfil1177@gmail.com>
2024-12-03 22:11:05 +0100

    (Day 3) Mull It Over (part 2)

--- tasks/src/day_3_mull_it_over/day_3.rs
+++ tasks/src/day_3_mull_it_over/day_3.rs
@@ -13,4 +13,32 @@ pub fn part_1() -> usize {
     })
 }
 
-pub fn part_2() {}
+pub fn part_2() -> usize {
+    let data = include_str!("./input.txt");
+
+    let re1 = Regex::new(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don\'t\(\)").unwrap();
+    let re2 = Regex::new(r"\d{1,3}").unwrap();
+
+    let mut is_skip = false;
+
+    re1.find_iter(data).fold(0, |acc, mat| {
+        let mat_str = mat.as_str();
+
+        match (is_skip, mat_str) {
+            (_, "do()") => {
+                is_skip = false;
+                acc
+            }
+            (true, _) => acc,
+            (false, "don't()") => {
+                is_skip = true;
+                acc
+            }
+            (false, _) => {
+                acc + re2
+                    .find_iter(mat_str)
+                    .fold(1, |acc, i| acc * i.as_str().parse::<usize>().unwrap())
+            }
+        }
+    })
+}
--- tasks/src/main.rs
+++ tasks/src/main.rs
@@ -4,6 +4,6 @@ use day_3_mull_it_over::day_3::part_1;
 use day_3_mull_it_over::day_3::part_2;
 
 fn main() {
-    println!("part_1 - {:?}", part_1());
-    // println!("part_2 - {:?}", part_2());
+    // println!("part_1 - {:?}", part_1());
+    println!("part_2 - {:?}", part_2());
 }
