309e290ca80076b57535c17aba0707245ab70e62
MakFil <makfil1177@gmail.com>
2024-12-15 21:40:32 +0100

    (Day 15) Warehouse Woes (part 2)

--- tasks/src/day_15_warehouse_woes/day_15.rs
+++ tasks/src/day_15_warehouse_woes/day_15.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashSet, VecDeque};
+
 #[derive(Debug)]
 enum Directions {
     Top,
@@ -123,9 +125,106 @@ impl Directions {
 
         true
     }
+
+    fn get_box_half_column(&self, position: (usize, usize), c: char) -> usize {
+        if c == '[' {
+            position.1 + 1
+        } else {
+            position.1 - 1
+        }
+    }
+
+    fn get_box_next_positions(&self, position: (usize, usize), c: char) -> Vec<(usize, usize)> {
+        let pos_1 = match self {
+            Directions::Top => (position.0 - 1, position.1),
+            Directions::Bottom => (position.0 + 1, position.1),
+            _ => position,
+        };
+
+        let pos_2 = (pos_1.0, self.get_box_half_column(position, c));
+
+        vec![pos_1, pos_2]
+    }
+
+    fn chart_incremental_update(
+        &self,
+        chart: &mut [Vec<char>],
+        position: (usize, usize),
+        positions_to_update: &mut VecDeque<(usize, usize)>,
+        positions_set: &mut HashSet<(usize, usize)>,
+    ) -> bool {
+        let c = chart[position.0][position.1];
+        match c {
+            '[' | ']' => match self {
+                Directions::Top | Directions::Bottom => {
+                    let second_half = (position.0, self.get_box_half_column(position, c));
+
+                    if positions_set.insert(position) && positions_set.insert(second_half) {
+                        let second_c = if c == '[' { ']' } else { '[' };
+                        let next_positions = [
+                            self.get_box_next_positions(position, c),
+                            self.get_box_next_positions(second_half, second_c),
+                        ];
+
+                        for &pos in next_positions.iter().flatten() {
+                            if !self.chart_incremental_update(
+                                chart,
+                                pos,
+                                positions_to_update,
+                                positions_set,
+                            ) {
+                                return false;
+                            }
+                        }
+
+                        positions_to_update.push_back(position);
+                        positions_to_update.push_back(second_half);
+                    }
+                }
+                _ => {
+                    let next_pos = self.get_new_position(position);
+                    if self.chart_incremental_update(
+                        chart,
+                        next_pos,
+                        positions_to_update,
+                        positions_set,
+                    ) {
+                        positions_to_update.push_back(position);
+                    } else {
+                        return false;
+                    }
+                }
+            },
+            '#' => {
+                return false;
+            }
+            _ => {
+                return true;
+            }
+        }
+        true
+    }
+
+    fn update_chart_with_shift_2(&self, chart: &mut [Vec<char>], position: (usize, usize)) -> bool {
+        let mut boxes_queue = VecDeque::<(usize, usize)>::new();
+        let mut positions_set = HashSet::new();
+
+        if !self.chart_incremental_update(chart, position, &mut boxes_queue, &mut positions_set) {
+            return false;
+        }
+
+        while let Some(item) = boxes_queue.pop_front() {
+            let new_pos = self.get_new_position(item);
+            let temp = chart[item.0][item.1];
+            chart[item.0][item.1] = chart[new_pos.0][new_pos.1];
+            chart[new_pos.0][new_pos.1] = temp
+        }
+
+        true
+    }
 }
 
-pub fn part_1() -> usize {
+fn get_res(is_part_2: bool) -> usize {
     let mut curr_position = (0, 0);
     let binding = include_str!("./input.txt").lines().collect::<Vec<_>>();
     let mut parts = binding.splitn(2, |&a| a.is_empty());
@@ -139,13 +238,25 @@ pub fn part_1() -> usize {
         .map(|(row_index, i)| {
             i.chars()
                 .enumerate()
-                .map(|(col_index, c)| {
-                    if c == '@' {
-                        curr_position.0 = row_index;
-                        curr_position.1 = col_index;
-                        return '.';
+                .flat_map(|(col_index, c)| {
+                    if is_part_2 {
+                        match c {
+                            '@' => {
+                                curr_position.0 = row_index;
+                                curr_position.1 = col_index * 2;
+                                vec!['.', '.']
+                            }
+                            'O' => vec!['[', ']'],
+                            v => vec![v, v],
+                        }
+                    } else {
+                        if c == '@' {
+                            curr_position.0 = row_index;
+                            curr_position.1 = col_index;
+                            return vec!['.'];
+                        }
+                        vec![c]
                     }
-                    c
                 })
                 .collect::<Vec<_>>()
         })
@@ -175,7 +286,11 @@ pub fn part_1() -> usize {
             continue;
         }
 
-        if chart_item == 'O' {
+        if is_part_2 && chart_item == '[' || chart_item == ']' {
+            if direction.update_chart_with_shift_2(&mut chart, new_position) {
+                curr_position = new_position;
+            }
+        } else if chart_item == 'O' {
             if direction.update_chart_with_shift(&mut chart, new_position) {
                 curr_position = new_position;
             }
@@ -186,7 +301,7 @@ pub fn part_1() -> usize {
 
     chart.iter().enumerate().fold(0, |acc, (row_index, row)| {
         acc + row.iter().enumerate().fold(0, |acc, (col_index, c)| {
-            if *c == 'O' {
+            if (is_part_2 && *c == '[') || *c == 'O' {
                 return acc + row_index * 100 + col_index;
             }
             acc
@@ -194,6 +309,10 @@ pub fn part_1() -> usize {
     })
 }
 
+pub fn part_1() -> usize {
+    get_res(false)
+}
+
 pub fn part_2() -> usize {
-    0
+    get_res(true)
 }
--- tasks/src/main.rs
+++ tasks/src/main.rs
@@ -5,5 +5,5 @@ use day_15_warehouse_woes::day_15::part_2;
 
 fn main() {
     println!("part_1 - {}", part_1());
-    // println!("part_2 - {}", part_2());
+    println!("part_2 - {}", part_2());
 }
