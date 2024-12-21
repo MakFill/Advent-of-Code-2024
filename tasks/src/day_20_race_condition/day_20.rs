548da490142a60a6af9e57134a3740e38727187d
MakFil <makfil1177@gmail.com>
2024-12-21 12:32:33 +0100

    (Day 20) Race Condition (part 2)

--- tasks/src/day_20_race_condition/day_20.rs
+++ tasks/src/day_20_race_condition/day_20.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 type Position = (usize, usize);
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
@@ -28,33 +26,62 @@ impl Directions {
             Directions::Left => vec![Directions::Top, Directions::Bottom, Directions::Left],
         }
     }
-
-    fn get_cheat_position(
-        &self,
-        pos: Position,
-        data: &[Vec<char>],
-        cheat_pos_set: &HashSet<(Position, Position)>,
-    ) -> Option<(Position, Position)> {
-        let obstacle_pos = self.get_next_position(pos);
-        if obstacle_pos.0 == 0
-            || obstacle_pos.0 == data.len() - 1
-            || obstacle_pos.1 == 0
-            || obstacle_pos.1 == data[0].len() - 1
-        {
-            return None;
-        }
-        let cheat_end_pos = self.get_next_position(obstacle_pos);
-        if data[cheat_end_pos.0][cheat_end_pos.1] == '.'
-            && !cheat_pos_set.contains(&(cheat_end_pos, pos))
-        {
-            Some((pos, cheat_end_pos))
-        } else {
-            None
-        }
-    }
 }
 
-pub fn part_1() -> usize {
+fn get_cheats_for_one_position(
+    position: Position,
+    data: &[Vec<char>],
+    dist_vec: &[Vec<i32>],
+    picoseconds_to_save: i32,
+    cheat_max_length: usize,
+) -> usize {
+    let mut res = 0;
+    let start_cost = dist_vec[position.0][position.1];
+    let row_first_index = if position.0 > cheat_max_length {
+        position.0 - cheat_max_length
+    } else {
+        1
+    };
+    let row_last_index = if position.0 + cheat_max_length < data.len() - 2 {
+        position.0 + cheat_max_length
+    } else {
+        data.len() - 2
+    };
+    let col_first_index = if position.1 > cheat_max_length {
+        position.1 - cheat_max_length
+    } else {
+        1
+    };
+    let col_last_index = if position.1 + cheat_max_length < data[0].len() - 2 {
+        position.1 + cheat_max_length
+    } else {
+        data[0].len() - 2
+    };
+
+    for row_index in row_first_index..=row_last_index {
+        for col_index in col_first_index..=col_last_index {
+            let c = data[row_index][col_index];
+            if c == '.' {
+                let end_cost = dist_vec[row_index][col_index];
+                if start_cost >= end_cost {
+                    continue;
+                }
+                let length =
+                    (position.0.abs_diff(row_index) + position.1.abs_diff(col_index)) as i32;
+
+                if length <= cheat_max_length as i32
+                    && end_cost - start_cost - length >= picoseconds_to_save
+                {
+                    res += 1;
+                }
+            }
+        }
+    }
+
+    res
+}
+
+fn get_result(picoseconds_to_save: i32, cheat_max_length: usize) -> usize {
     let mut start = (0, 0);
     let mut finish = (0, 0);
     let data = include_str!("./input.txt")
@@ -81,7 +108,6 @@ pub fn part_1() -> usize {
     let rows = data.len();
     let cols = data[0].len();
     let mut dist_vec = vec![vec![0; cols]; rows];
-    let mut cheat_pos_set = HashSet::new();
 
     let mut curr = start;
     let all_directions = [
@@ -95,9 +121,6 @@ pub fn part_1() -> usize {
         .filter(|d| {
             let next_pos = d.get_next_position(curr);
             if data[next_pos.0][next_pos.1] == '#' {
-                if let Some(cheat_pos) = d.get_cheat_position(start, &data, &cheat_pos_set) {
-                    cheat_pos_set.insert(cheat_pos);
-                }
                 return false;
             }
 
@@ -113,9 +136,6 @@ pub fn part_1() -> usize {
             .filter_map(|d| {
                 let next_pos = d.get_next_position(curr);
                 if data[next_pos.0][next_pos.1] == '#' {
-                    if let Some(cheat_pos) = d.get_cheat_position(curr, &data, &cheat_pos_set) {
-                        cheat_pos_set.insert(cheat_pos);
-                    }
                     None
                 } else {
                     Some((next_pos, d))
@@ -129,12 +149,29 @@ pub fn part_1() -> usize {
         dist_vec[curr.0][curr.1] = distance;
     }
 
-    cheat_pos_set.iter().fold(0, |acc, (start, end)| {
-        let cheat_distance = dist_vec[end.0][end.1] - dist_vec[start.0][start.1] - 2;
-        acc + if cheat_distance >= 100 { 1 } else { 0 }
-    })
+    let mut res = 0;
+    for row_index in 1..rows - 1 {
+        for col_index in 1..cols - 1 {
+            let c = data[row_index][col_index];
+            if c == '.' {
+                res += get_cheats_for_one_position(
+                    (row_index, col_index),
+                    &data,
+                    &dist_vec,
+                    picoseconds_to_save,
+                    cheat_max_length,
+                );
+            }
+        }
+    }
+
+    res
+}
+
+pub fn part_1() -> usize {
+    get_result(100, 2)
 }
 
 pub fn part_2() -> usize {
-    0
+    get_result(100, 20)
 }
--- tasks/src/main.rs
+++ tasks/src/main.rs
@@ -1,9 +1,9 @@
 mod day_20_race_condition;
 
 use day_20_race_condition::day_20::part_1;
-// use day_20_race_condition::day_20::part_2;
+use day_20_race_condition::day_20::part_2;
 
 fn main() {
     println!("part_1 - {}", part_1());
-    // println!("part_2 - {}", part_2());
+    println!("part_2 - {}", part_2());
 }
